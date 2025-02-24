c7aa39a54438f3509502faee5c361e3504a467d7
AmendanoLuis <1uis4ugus70@gmail.com>
2025-02-24 17:34:44 +0100

    Implementar un nuevo script API y actualizar la llamada AJAX para la recuperación de datos de caracteres

---
+++ api_rick_morty.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Archivo: api_rick_morty.php
+ * Descripción: Script PHP que actúa como intermediario para obtener datos de personajes de la API de Rick and Morty.
+ * Devuelve una respuesta en formato JSON con la información obtenida de la API externa.
+ *
+ * @author Luis Augusto Procel Amendaño
+ * @version 1.0
+ */
+
+header("Content-Type: application/json");
+
+/**
+ * Obtiene la información de un personaje de la API de Rick and Morty.
+ *
+ * @param string $name Nombre del personaje a buscar.
+ * @return string JSON con la información del personaje o un mensaje de error.
+ */
+function getCharacterData($name) {
+    /**
+     * @var string $api_url URL de la API con el parámetro de búsqueda
+     */
+    $api_url = "https://rickandmortyapi.com/api/character/?name=" . urlencode($name);
+
+    /**
+     * @var string|false $response Respuesta JSON obtenida de la API
+     */
+    $response = file_get_contents($api_url);
+
+    return $response ?: json_encode(["error" => "No se pudieron obtener los datos."]);
+}
+
+/**
+ * Verifica si se ha proporcionado un parámetro 'name' en la URL y devuelve la información del personaje.
+ *
+ * @return void
+ */
+function handleRequest() {
+    if (isset($_GET['name']) && !empty($_GET['name'])) {
+        echo getCharacterData($_GET['name']);
+    } else {
+        echo json_encode(["error" => "Parámetro 'name' no proporcionado"]);
+    }
+}
+
+handleRequest();
---
+++ doc/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\getCharacterData\u0028\u0029",
+            "name": "getCharacterData",
+            "summary": "Obtiene\u0020la\u0020informaci\u00F3n\u0020de\u0020un\u0020personaje\u0020de\u0020la\u0020API\u0020de\u0020Rick\u0020and\u0020Morty.",
+            "url": "namespaces/default.html#function_getCharacterData"
+        },                {
+            "fqsen": "\\handleRequest\u0028\u0029",
+            "name": "handleRequest",
+            "summary": "Verifica\u0020si\u0020se\u0020ha\u0020proporcionado\u0020un\u0020par\u00E1metro\u0020\u0027name\u0027\u0020en\u0020la\u0020URL\u0020y\u0020devuelve\u0020la\u0020informaci\u00F3n\u0020del\u0020personaje.",
+            "url": "namespaces/default.html#function_handleRequest"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
